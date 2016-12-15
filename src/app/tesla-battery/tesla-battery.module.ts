2501496801d3b6615253ed6f042e301a98b6417f
ryan.waskiewicz <ryanwaskiewicz@gmail.com>
2016-12-14 22:25:08 -0500

    Through wiring up battery component an redoing home page

---
+++ src/app/tesla-battery/tesla-battery.module.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { BatteryService } from './tesla-battery.service';
+import { TeslaBatteryComponent } from './containers/tesla-battery/tesla-battery.component';
+
+@NgModule({
+  declarations: [
+    TeslaBatteryComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    BatteryService
+  ],
+  exports: [
+    // root module will use this component eventually
+    TeslaBatteryComponent
+  ]
+})
+export class TeslaBatteryModule {}
