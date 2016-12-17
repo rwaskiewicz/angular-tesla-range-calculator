00bb11b0daca8915b470000b31bd55cab6b5de2c
ryan.waskiewicz <ryanwaskiewicz@gmail.com>
2016-12-17 14:19:34 -0500

    Add speed and temp

--- src/app/tesla-battery/tesla-battery.module.ts
+++ src/app/tesla-battery/tesla-battery.module.ts
@@ -7,12 +7,14 @@ import { TeslaBatteryComponent } from './containers/tesla-battery/tesla-battery.
 
 import { TeslaCarComponent } from './components/tesla-car/tesla-car.component';
 import { TeslaStatsComponent } from './components/tesla-stats/tesla-stats.component';
+import { TeslaCounterComponent } from './components/tesla-counter/tesla-counter.component';
 
 @NgModule({
   declarations: [
     TeslaBatteryComponent,
     TeslaCarComponent,
-    TeslaStatsComponent
+    TeslaStatsComponent,
+    TeslaCounterComponent
   ],
   imports: [
     CommonModule,
